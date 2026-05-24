1becbe5cbb6fe9363288b6457d893196af1b42ce
Kalo-Green <kalogreenstyle@gmail.com>
2026-05-24 07:16:04 +0200

    setup HTML CSS JS base layout

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
+
+let tasks = [];
