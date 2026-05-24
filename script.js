d1905ed6a965d2e4ff87278aca1536c8f4d33bc3
Kalo-Green <kalogreenstyle@gmail.com>
2026-05-24 08:27:39 +0200

    add task creation and rendering

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 "use strict";
 
-const form = document.getElementById("todo-form");
-const input = document.getElementById("todo-input");
-const list = document.getElementById("todo-list");
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
 
 let tasks = [];
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const text = todoInput.value.trim();
+    if (!text) return;
+
+    tasks.push(text);
+
+    todoInput.value = "";
+
+    renderTasks();
+});
+
+function renderTasks() {
+    todoList.innerHTML = "";
+
+    tasks.forEach((task) => {
+        const li = document.createElement("li");
+        li.textContent = task;
+
+        todoList.appendChild(li);
+        console.log("list : " + todoList);
+    });
+
+    console.log(tasks);
+    
+    
+}
+
